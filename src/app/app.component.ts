e6a9b2cf96e872dcddca2523da43dd2e516a30c6
tommaso_lazzaretti <tommaso.lazzaretti@corley.it>
2023-11-28 14:48:58 +0100

    (feat): added tailwind configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-skeleton';
 
   showModal = false;
 
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
   onContinue() {
     // Gestisci l'azione di continuazione qui
+    this.showModal = false;
   }
 
   onCancel() {
     // Gestisci l'azione di cancellazione qui
+    this.showModal = false;
   }
 
   customAction() {
     // Gestisci l'azione personalizzata qui
+    this.showModal = false;
   }
 }
--- src/app/components/atoms/button/button.component.ts
+++ src/app/components/atoms/button/button.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ButtonComponent {
+
+  @Output() onClickEvent: EventEmitter<any> = new EventEmitter();
+
+  onClick = () => {
+    this.onClickEvent.emit()
+  }
 
 }
